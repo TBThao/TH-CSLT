ef5c9fa9b8e42af74e2873e2310301883f585338
TBThao <133199790+TBThao@users.noreply.github.com>
2025-11-20 09:31:22 +0700

    Add files via upload

---
+++ lab_06/lab_06/lab_06/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+#include "fraction_helper/fraction_utils.h"
+#include "student_helper/student_utils.h"
+
+using namespace std;
+
+// TODO
+
+// END TODO
+
+int main()
+{
+    // TODO
+    auto_test_fraction();
+    int t;
+    while(t--){
+        char c;
+        cin >> c;
+        string s = "";
+        getline(cin, s);
+
+    }
+    // END TODO
+    return 0;
+}
